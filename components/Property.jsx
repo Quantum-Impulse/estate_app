82f64b0e1fc29de52943c14e76ec5c00859daa74
Enrique Rivera <erivera7240@gmail.com>
2021-12-18 03:13:17 -0600

    adding prices and rates

--- components/Property.jsx
+++ components/Property.jsx
@@ -5,10 +5,23 @@ import {FaBed, FaBath} from 'react-icons/fa';
 import {BsGridFill} from 'react-icons/bs';
 import {GoVerified} from 'react-icons/go';
 import millify from 'millify';
+import DefaultImage from '../assets/images/house.jpg';
 
-const Proverty = ({property: {coverPhoto, price, rentFrequecy, rooms, title, baths, area, agency, isVerified, externalID} }) => (
+const Proverty = ({property: {coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID} }) => (
     <Link href={`/property/${externalID} passHref`}>
-        {title}
+        <Flex flexWrap="wrap" w="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
+            <Box>
+                <Image src={coverPhoto ? coverPhoto.url : DefaultImage} width={400} height={260} alt='house'/>
+            </Box>
+            <Box w="full" >
+                <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+                    <Flex alignContent="center">
+                        <Box paddingRight="3" color="green.400">{isVerified && <GoVerified />}</Box>
+                        <Text fontWeight="bold" fontSize="lg">AED {millify(price)}{rentFrequency && `/${rentFrequency}`}</Text>
+                    </Flex>
+                </Flex>
+            </Box>
+        </Flex>
     </Link>
 );
 
